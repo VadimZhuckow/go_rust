d17b31b9cc50c862cfd2643c1a6bad72be88cef2
VadimZhuckow <apex69knight@gmail.com>
2024-12-02 19:04:31 +0300

    add rand

--- hello_cargo/guissing_game/src/main.rs
+++ hello_cargo/guissing_game/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use rand::Rng;
+
+fn main(){
+    println!("Игра угадай число");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    
+    println!("The secret number is: {secret_number}");
+    
+    println!("Введите число");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Ошибка чтения");
+
+    println!("Ваше число: {}", guess)
+
+    
 }
